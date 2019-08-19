7bdd4b96dc8b2aea996e732c18244aa278f2ddc2
Anthony Nelzin-Santos <anthony@nelzin.fr>
2019-08-19 16:28:40 +0200

    Ajouter __main__.py pour exécution dans scripts
    Signed-off-by: Anthony Nelzin-Santos <anthony@nelzin.fr>

---
+++ __main__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""
+    Anthony Nelzin-Santos
+    anthony@nelzin.fr
+    https://anthony.nelzin.fr
+
+    European Union Public License 1.2
+"""
+
+from windowStitcher import main
+
+if __name__ == '__main__':
+    main()
